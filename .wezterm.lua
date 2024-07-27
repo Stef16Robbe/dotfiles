5cfedc005b9a54e0a47eeff26a86e282628a860e
Stef <stef.robbe@protonmail.com>
2024-07-27 19:34:16 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,15 @@ config.font = wezterm.font "Berkeley Mono"
 config.color_scheme = "Batman"
 config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 config.default_cwd = "~/proj"
+config.keys = {
+  {
+    key = "w",
+    mods = "CTRL|SHIFT",
+    -- mods = "CMD",
+    action = wezterm.action.CloseCurrentTab { confirm = false },
+  },
+}
+config.scrollback_lines = 10000
 
 -- and finally, return the configuration to wezterm
 return config
