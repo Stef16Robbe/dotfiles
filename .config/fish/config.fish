75a4358cb8b121cd0d86244e8ded651d41fc0388
Stef <stef.robbe@protonmail.com>
2024-12-06 14:06:22 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,18 @@ source (dirname (status --current-filename))/aliases.fish
 #    source (dirname (status --current-filename))/config-osx.fish
 #  case '*'
 #    ...
+
+# abbreviations
+abbr --add gch 'git checkout'
+abbr --add gco 'git commit -m'
+abbr --add coc 'cog commit'
+abbr --add gps 'git push'
+abbr --add gs 'git status'
+abbr --add gpl 'git pull'
+abbr --add gb 'git branch'
+abbr --add grs 'git restore'
+abbr --add grm 'git remote'
+abbr --add gm 'git merge'
+abbr --add ga 'git add'
+abbr --add kb "kustomize build"
+abbr --add gd "git diff"
